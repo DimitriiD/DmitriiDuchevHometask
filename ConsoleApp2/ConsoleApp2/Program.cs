9be201ca0522242313a2531ff6964ae58d85a332
DimitriiD <duchevd@yandex.ru>
2020-04-20 04:05:09 +0300

    stroka and massiv

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            Random mrnd = new Random();
+            int[] arr = new int[15];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = mrnd.Next(0, 1000);
+                Console.WriteLine("arr[{0}] = {1}", i, arr[i]);
+                if (arr[i] % 2 == 0)
+                {
+                    x += arr[i];
+                }
+            }
+            Console.WriteLine("x = {0}", x);
+        }
+    }
+}
